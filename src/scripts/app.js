7b1f631e5c20097a1cde7cb145500d8ffde8edae
Maria Tarasenko <mariaatarasenko@gmail.com>
2018-01-04 01:57:51 +0300

    changed video + added menu functions

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,3 +9,34 @@
 });
 })();
 
+(function(){
+    const blogBtn =document.querySelector('.mobile_articles_button');
+    const blogMenu =document.querySelector('..mobile_articles');
+
+    blogBtn.addEventListener('click',()=>{
+        blogBtn.classList.toggle('active');
+        blogMenu.classList.toggle('active');
+        
+    });
+    
+})();
+
+$(document).ready(function() {
+// opened burger-menu
+$('.hamburger').on('click', (e) => {
+    e.preventDefault();
+    $('.nav__menu_hidden').toggleClass('open');
+    $('.hamburger').css({
+        'z-index': 101
+    });
+    
+});
+$(".hamburger").on("click", function() {
+    const that = $(this);    
+    if (that.hasClass("is-open")) {
+      that.removeClass("is-open").addClass("is-closed");      
+    } else {
+      that.removeClass("is-closed").addClass("is-open");      
+    }    
+  });
+})();
