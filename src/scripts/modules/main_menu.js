6e4f02ea901eb85a3d56641fc2b93cf254c2a540
Maria Tarasenko <mariaatarasenko@gmail.com>
2018-01-10 16:58:10 +0300

    added video for each page, changed main-menu

--- src/scripts/modules/main_menu.js
+++ src/scripts/modules/main_menu.js
@@ -3,7 +3,15 @@ module.exports = function(){
     // opened burger-menu
     $('.hamburger').on('click', (e) => {
         e.preventDefault();
-        $('.nav__menu_hidden').toggleClass('open');
+        
+        $('.background__left').toggleClass('active');
+        $('.backround__right').toggleClass('active');
+       
+        setTimeout(function(){ 
+             $('.nav__menu_hidden').addClass('open');}, 2000); 
+    
+        //$('.nav__menu_hidden').fadeIn('2000');
+        
         $('.hamburger').css({
             'z-index': 201
         });
@@ -12,9 +20,15 @@ module.exports = function(){
     $(".hamburger").on("click", function() {
         const that = $(this);    
         if (that.hasClass("is-open")) {
-          that.removeClass("is-open").addClass("is-closed");      
-        } else {
-          that.removeClass("is-closed").addClass("is-open");      
+          that.removeClass("is-open").addClass("is-closed"); 
+          
+          setTimeout(function(){ 
+            $('.nav__menu_hidden').removeClass('open');}, 2000);          
+        }   
+         else { 
+          that.removeClass("is-closed").addClass("is-open");
+          setTimeout(function(){ 
+            $('.nav__menu_hidden').addClass('open');}, 1000);          
         }    
       });
    
